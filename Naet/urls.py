2ef159b0416c8f9eeb8331b4fbdf96881d777410
thelaughingbett <bett3304@gmail.com>
2026-09-14 09:57:55 +0300

    some corrections here and there

--- Naet/urls.py
+++ Naet/urls.py
@@ -3,10 +3,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from staffConsole.views import ComingSoonView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
     path('staff/', include('staffConsole.urls')),
+
+    path(
+        'coming-soon',
+        ComingSoonView.as_view(),
+        name='coming-soon'
+    )
 ]
 
 
--- staffConsole/urls/__init__.py
+++ staffConsole/urls/__init__.py
@@ -4,10 +4,4 @@ from staffConsole.views import ComingSoonView
 urlpatterns = [
     path('lecturer/', include('staffConsole.urls.lecturer')),
     path('hod/', include('staffConsole.urls.hod')),
-
-    path(
-        'coming-soon',
-        ComingSoonView.as_view(),
-        name='coming-soon'
-    )
 ]
